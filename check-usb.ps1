f0f689592e05568757d3c36331dd946521bf23af
DateFix <157734918+DateFix@users.noreply.github.com>
2026-03-29 08:14:47 +0200

    Delete check-usb.ps1

--- check-usb.ps1
+++
@@ -1,8 +0,0 @@
-$path = "HKLM:\SYSTEM\CurrentControlSet\Services\USBSTOR"
-$value = (Get-ItemProperty -Path $path).Start
-
-if ($value -eq 4) {
-    Write-Output "USB le van tiltva"
-} else {
-    Write-Output "USB engedélyezve"
-}
