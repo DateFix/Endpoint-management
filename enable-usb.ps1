6b1422dc8d337d80c80d6e412bbafff7bd56680c
DateFix <157734918+DateFix@users.noreply.github.com>
2026-03-29 08:15:05 +0200

    Delete enable-usb.ps1

--- enable-usb.ps1
+++
@@ -1,9 +0,0 @@
-$logFile = "C:\temp\usb_status.log"
-$date = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-
-$path = "HKLM:\SYSTEM\CurrentControlSet\Services\USBSTOR"
-$value = (Get-ItemProperty -Path $path).Start
-
-$status = if ($value -eq 4) { "Disabled" } else { "Enabled" }
-
-"$date - USB Status: $status" | Out-File -Append $logFile
